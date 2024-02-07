464eb41a04f1bc33c0223ab4eb44f0cf14b2cbeb
Cecilia Norevik Bratlie <cecilia_nor@hotmail.com>
2024-02-07 16:23:20 +0100

    feat: get style.json data from mapTiler API and draw its map tile.
    Multiple comments have also been added to main.
    
    Updates: #26.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <QImage>
 #include <QApplication>
 
-// #include "httpHandling/networkcontroller.h" // Get JSON syle data from Maptiler
+#include "httpHandling/networkcontroller.h" // Get JSON syle data from Maptiler
 
 #include "tilerenderrules.h"
 #include "vectortile.h"
@@ -10,11 +10,12 @@
 #include "vector_tile.qpb.h"
 
 
-// const QString url ="https://api.maptiler.com/maps/streets/style.json?key=bWo4cKyYIs8K3SkrLiTk"; // Constant url for testing
+const QString url ="https://api.maptiler.com/maps/streets/style.json?key=bWo4cKyYIs8K3SkrLiTk"; // Constant url for testing
 
+const QString url2 ="https://api.maptiler.com/maps/basic-v2/style.json?key=bWo4cKyYIs8K3SkrLiTk"; // Const url for testing against Matthias' example
 int main(int argc, char *argv[])
 {
-    /*
+
     // HTTP Setup
     //
     // Commented away to not make unnecessary requests to MapTiler.
@@ -26,20 +27,20 @@ int main(int argc, char *argv[])
     // Set up the httpHandler
     NetworkController networkController;
 
-    // Set up request to httpHandler
-    //QNetworkRequest request(url);
-
     // Make http request to get a json stylesheet
-    QByteArray responseData = networkController.sendRequest(url);
-    qDebug() << "Response: " << responseData;
-    */
+    //QByteArray responseData = networkController.sendRequest(url2);
+    //qDebug() << "Response: " << responseData;
 
     QJsonDocument doc;
     QJsonParseError parseError;
 
+    // Loading file from disk
     QFile fStyle(":/testData/tiles.json");
     fStyle.open(QIODevice::ReadOnly);
     doc = QJsonDocument::fromJson(fStyle.readAll(), &parseError);
+
+    // Using data from MapTiler API
+    //doc = QJsonDocument::fromJson(responseData, &parseError);
 
     if (parseError.error != QJsonParseError::NoError) {
         qWarning() << "Parse error at" << parseError.offset << ":" << parseError.errorString();
@@ -48,10 +49,16 @@ int main(int argc, char *argv[])
 
     TileRenderRules *rules = TileRenderRules::fromJson(doc);
 
+    // Read map vector tile from file
     QFile fTile(":/testData/z12x2170y1190.mvt");
     fTile.open(QIODevice::ReadOnly);
     QByteArray data = fTile.readAll();
 
+
+    /*TODO: Get map vector tile by making a get request to mapTiler API*/
+    // Eimen's code (or maybe Cecilia's) could/should go here.
+
+    // Generate a tile to draw
     VectorTile tile;
     tile.load(data);
 
@@ -66,5 +73,5 @@ int main(int argc, char *argv[])
     renderer.m_zoomLevel = 12;
     renderer.render(&p, tile);
 
-//    image.save("C:\\Users\\cecil\\Desktop\\z12x2170y1190.png");
+    //image.save("C:\\Users\\cecil\\Desktop\\test3-img.png");
 }
