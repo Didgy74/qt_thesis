630cf4a6c79b12cc8eb5725777e49f24bdbba51a
Cecilia Norevik Bratlie <cecilia_nor@hotmail.com>
2024-03-11 16:23:56 +0100

    chore: remove redundant comments

--- app/main.cpp
+++ app/main.cpp
@@ -98,8 +98,6 @@ int main(int argc, char *argv[])
     auto &styleSheet = mapWidget->styleSheet;
     auto &tileStorage = mapWidget->tileStorage;
 
-
-    /// REFACTOR HERE. Cecilia will discuss on an upcoming meeting how to handle this differently.
     // Parses the bytes that form the stylesheet into a json-document object.
     QJsonParseError parseError;
     auto styleSheetJsonDoc = QJsonDocument::fromJson(styleSheetBytes.response, &parseError);
@@ -114,7 +112,6 @@ int main(int argc, char *argv[])
     }
     // Then finally parse the JSonDocument into our StyleSheet.
     styleSheet.parseSheet(styleSheetJsonDoc);
-    /// REFACTOR END
 
     auto downloadTile = [&](TileCoord tile) -> VectorTile {
         auto tileLink = tileLoader.downloadTile(Bach::setPbfLink(tile, pbfLinkTemplate.link));
